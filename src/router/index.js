76c3c4abad860182e9ffa2857204be126e35c9ec
Michael Nix <mpnix@protonmail.com>
2021-03-06 23:27:11 +0000

    Updated routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Projects from '../views/Projects.vue'
 import About from '../views/About.vue'
 import Contact from '../views/Contact.vue'
-// import ThankYou from '../views/ThankYou.vue'
+import ThankYou from '../views/ThankYou.vue'
 
 const routes = [
   {
@@ -14,11 +14,20 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/contact',
     name: 'Contact',
     component: Contact
+  },
+  {
+    path: '/thankyou',
+    name: 'ThankYou',
+    component: ThankYou
   }
 ]
 
