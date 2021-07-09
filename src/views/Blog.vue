4d117a09e09b760fabafb3fb3eff5baa07c8de57
Michael Nix <60673454+c0llid3r@users.noreply.github.com>
2021-07-09 23:54:39 +0100

    Add blog component

---
+++ src/views/Blog.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id='blog' class='container blog-container'>
+        <div class="post" v-for="post in posts.reverse()" :key="post">
+            <div>{{post.title}}</div>
+
+            <img
+                :src="'https://dropbox.com/s/' + post.image + '.png'"
+                :alt="post.title" />
+
+            <div><p v-html="post.body"></p></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            posts: []
+        }
+    },
+    methods: {
+        async getPosts () {
+           const response = await fetch('https://nixwebdev-api.herokuapp.com/api/data');
+           const posts = await response.json();
+           this.posts = posts;
+        }
+    },
+    mounted () {
+        this.getPosts();
+    }
+}
+</script>
