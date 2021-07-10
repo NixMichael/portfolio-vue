1af394d1a110db7d3f4dc020c568658a61e6a8cd
Michael Nix <mpnix@protonmail.com>
2021-07-10 20:27:54 +0100

    Updated blog component description/intro

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -2,6 +2,7 @@
     <div id='blog' class='container blog-container'>
         <div class='text-content'>
         <h1><span class='title-font highlight-color'>Blog</span></h1>
+        <p>Thoughts and findings relating to web development, Linux, and anything else of interest to me.</p>
         <div class="post" v-for="post in posts.reverse()" :key="post">
             <div>{{post.title}} <span>({{post.created_at}})</span></div>
 
