cd0834813f345485d5ee6e47b85cc6167c42daba
Michael Nix <mpnix@protonmail.com>
2021-07-13 00:16:20 +0100

    Updated Blog data urls

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -3,10 +3,10 @@
         <div class='text-content'>
         <h1><span class='title-font highlight-color'>Blog</span></h1>
         <p>Thoughts and findings relating to web development, Linux, and anything else of interest to me.</p>
-        <div class="post" v-for="post in posts.reverse()" :key="post">
+        <div class="post" v-for="post in posts" :key="post">
             <div>{{post.title}}<span>({{post.created_at}})</span></div>
             <img 
-                :srcset="'https://nixwebdev.s3.eu-west-2.amazonaws.com/blogimages/' + post.image + '.png' + ' 1359'"
+                :srcset="'https://nixwebdev.s3.eu-west-2.amazonaws.com/blogimages/' + post.image + '.webp' + ' 1359'"
                 :src="'https://nixwebdev.s3.eu-west-2.amazonaws.com/blogimages/' + post.image + '.png'" 
                 :alt="post.title" />
 
@@ -18,16 +18,16 @@
 
 <script>
 export default {
-    data () {
+    data () {api
         return {
             posts: []
         }
     },
     methods: {
         async getPosts () {
-           const response = await fetch('http://localhost:8000/api/data');
+           const response = await fetch('https://nixwebdev-api.herokuapp.com/api/data');
            const posts = await response.json();
-           this.posts = posts;
+           this.posts = posts.reverse();
         }
     },
     mounted () {
