dfbef4e296663eceebe32ba36012f792ae166078
Michael Nix <mpnix@protonmail.com>
2021-07-12 13:18:03 +0100

    Updated img src urls for blog post image

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -6,8 +6,8 @@
         <div class="post" v-for="post in posts.reverse()" :key="post">
             <div>{{post.title}}<span>({{post.created_at}})</span></div>
             <img 
-                :srcset="'https://nixwebdev.s3.eu-west-2.amazonaws.com/' + post.image + '.webp' + ' 1359'"
-                :src="'https://nixwebdev.s3.eu-west-2.amazonaws.com/' + post.image + '.png'" 
+                :srcset="'https://nixwebdev.s3.eu-west-2.amazonaws.com/blogimages/' + post.image + '.png' + ' 1359'"
+                :src="'https://nixwebdev.s3.eu-west-2.amazonaws.com/blogimages/' + post.image + '.png'" 
                 :alt="post.title" />
 
             <div><p v-html="post.body"></p></div>
@@ -25,7 +25,7 @@ export default {
     },
     methods: {
         async getPosts () {
-           const response = await fetch('https://nixwebdev-api.herokuapp.com/api/data');
+           const response = await fetch('http://localhost:8000/api/data');
            const posts = await response.json();
            this.posts = posts;
         }
